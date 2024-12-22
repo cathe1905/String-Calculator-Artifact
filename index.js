2c65d55c4fec332713d857c0e7a9c1737f50cd54
catherin <catherinr24@gmail.com>
2024-12-22 18:17:31 -0300

    Extended Add method to support an unknown number of inputs.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// SIMPLE SUMMATION: This function will be responsible for summing all numbers within a string, with  0 and 2 elements.
+// INFINITIVE ARITHMETIC: This function will be responsible for summing all numbers within a string, and support an unknown number of inputs.
 function intAdd (str){
 
     // Evaluate if str is an empty string
@@ -9,13 +9,15 @@ function intAdd (str){
     //Transform the string into an array by separating them with a comma.
     const newArray= str.split(",");
     
-    //Evaluate if newArray contains one or two elements and then make the sum
-    switch(newArray.length){
-        case 1:
-            return Number(newArray[0]);
-        case 2:
-            return Number(newArray[0]) + Number(newArray[1]);
+    //variable for the result
+    let result= 0;
+
+    // Iteration for summing all elementes within newArray
+    for(let i=0; i < newArray.length; i++){
+        result += Number(newArray[i])
     }
+
+    return result;
 
 }
 
@@ -23,10 +25,14 @@ function intAdd (str){
 console.log(intAdd(""))
 console.log(intAdd("1"))
 console.log(intAdd("1,2"))
-console.log(intAdd("30,55"))
-console.log(intAdd("17"))
+console.log(intAdd("30,30, 40, 50, 100, 25, 10"))
+console.log(intAdd("17, 3, 20"))
+console.log(intAdd("5,12,18,20,25,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250,260,270,280,290,300"));
+
 
 // Technical Justification of the Solution
-// I decided to emphasize simplicity because the function will only
-// receive 0, one, or two elements. It's not worth using methods or loops 
-// for just one sum.
+// I made a modification to the previous function by replacing the switch with a loop. 
+// This change was necessary because I didn't know how many numbers would be in the string.
+// A loop is better option than other JavaScript methods because it's a simple solution and reduces
+// the execution time.
+
