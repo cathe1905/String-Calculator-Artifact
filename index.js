264e3a39ebe619cea53222560220fb7722c62852
catherin <catherinr24@gmail.com>
2024-12-29 23:38:23 -0300

    Implemented exception handling for negative numbers.

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-// CUSTOM DELIMITERS: This function will handle custom delimiters
-// defined in a specific format, which will be evaluated at the beginning
-// while still supporting the default delimiters (commas and newlines).
+// NEGATIVE REBELION: This function will maintain the previous functionalities, with the particularity 
+// that it will perform an additional evaluation, and if there are negative numbers, it will throw 
+// an exception.
 
 
 function intAdd (str){
+
+    //VARIABLES
+
+    let negatives= []; //to save negative numbers
+    let custom= ""; //Declare the custom variable
+    let temporaryContainer=""; // Temporary container for data
+    let result= 0; //variable for the result
+
 
     // Evaluate if str is an empty string
     if(str === ""){
         return 0;
     }
-
-    //Declare the custom variable
-    let custom= "";
-
+ 
     // Evaluate whether the string contains the required format for custom delimiters without using regular expressions.
     if(str[0] === "/" && str[1] === "/" && str[3] === "\n"){
         custom= str[2];
     }
-    
-    //variable for the result
-    let result= 0;
-
-    // Temporary container for data
-    let temporaryContainer="";
-    
+     
    // Iterates to select the characters that are numbers and compute the sum
     for(let i=0; i < str.length; i++){
 
@@ -32,17 +31,26 @@ function intAdd (str){
             // Temporarily save the number in a container
             temporaryContainer+= str[i];
         }else{
-            // If it's not a number, it means it's a delimiter. Close the current number and add it to the result.
-            result+= Number(temporaryContainer);
+            // If it's not a number, it means it's a delimiter. Close the current number and add it to the result, as long as it's not a negative number
+            Number(temporaryContainer) < 0 ? negatives.push(Number(temporaryContainer)) : result+= Number(temporaryContainer)
             // Clear the temporary container
             temporaryContainer= ""
-            
-        }
-        
+        } 
     }
-    // If the last character was a number, it means the container still holds one number. 
-    // Add it to the result; otherwise, return the result.
-    return temporaryContainer.length > 0 ? result += Number(temporaryContainer) : result;
+
+    // If the last character was a number, it means the container still holds one number.
+    result += Number(temporaryContainer) 
+
+    //If the array of negative numbers contains elements, process the last digit in temporaryContainer and throw the exception.
+    if(negatives.length > 0 ){
+        if(temporaryContainer.length > 0 && Number(temporaryContainer) < 0){
+            negatives.push(Number(temporaryContainer))
+        }
+        console.error("negatives not allowed:" + negatives.join(","))
+        return 
+    }
+    
+    return result;
 
 }
 
@@ -57,17 +65,16 @@ console.log(intAdd("15\n25,35\n45,55\n65,75,85\n95,105,115\n125,135,145,155,165,
 console.log(intAdd("//;\n1;3;2.5;3")) // Handling cases with decimal numbers
 console.log(intAdd("//*\n1*2"))
 console.log(intAdd("//?\n1?20"))
+console.log(intAdd("1,-2,-3"))
+console.log(intAdd("//*\n-1*2")) // Handling cases with custom delimiters and negative numbers together
 
 
 
 // Technical justification of the solution
 
-// In this addition of new functionality to the intAdd function, I continued with the approach of simple procedures to ensure 
-// the function remains fast and efficient. At the beginning, I used an if statement to check whether the input complies with 
-// the custom delimiter format. During the for loop, I added an evaluation for the "/" character and the content of the custom 
-// variable. If no custom delimiter is defined, the evaluation remains unchanged and continues to fulfill the previous 
-// functionalities. At one point, I considered using isNaN, but I decided against it because when handling a decimal number, 
-// the period (.) would be excluded as part of the number. For this reason, I am currently maintaining this method to 
-// distinguish between numbers and delimiters or parts of a format.
+// In this new functionality, I added a simple validation in the same section of the for loop to avoid long code. I kept the evaluation
+// within the for loop for cases where a combination might arise, such as an input with a custom delimiter format and negative 
+// numbers. This approach allows me to evaluate all possible scenarios where negative numbers appear, while keeping the code short 
+// and understandable
 
 
