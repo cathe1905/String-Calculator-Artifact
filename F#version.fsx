43f7472fba01ea9a415e9743ae657949da356e75
catherin <catherinr24@gmail.com>
2025-01-04 23:16:27 -0300

    Extended Add method to support an unknown number of inputs. (F# VERSION)

--- F#version.fsx
+++ F#version.fsx
@@ -1,34 +1,35 @@
-// SIMPLE SUMMATION: This function will be responsible for summing all numbers within a string, 
-//between  0 and 2 elements.
+// INFINITIVE ARITHMETIC: This function will be responsible for summing all numbers within a string, and support 
+//an unknown number of inputs.
 
 //this function will return an integer
 let intAdd (str) : int =
+
     // Evaluate if str is an empty string
     if str = "" then 
         0 
     else 
-        // Splits the string 'str' into a list of substrings using the comma as the delimiter.
+        // Splits the string 'str' into an array of substrings using the comma as the delimiter.
         let array = str.Split(",")
 
-         //Evaluate if newArray contains one or two elements and then make the sum
-        if array.Length = 1 then
-            let result = (int array.[0])
-            result
-        else if array.Length = 2 then
-            let result = (int array.[0]) + (int array.[1])
-            result
-        else 
-            printfn "%s" "invalid input"
-            0 
+        // Convert each element of the array into a number.
+        let num= Array.map int array
 
+        //Use the sum function to add all the integers.
+        let sum= Array.sum num
+
+        //Return the result.
+        sum
+        
+       
 // Tests
 printfn "Test 1: %d" (intAdd "")
-printfn "Test 2: %d" (intAdd "1,3")
-printfn "Test 3: %d" (intAdd "1,2")
-printfn "Test 3: %d" (intAdd "30,55")
+printfn "Test 2: %d" (intAdd "30,30, 40, 50, 100, 25, 10")
+printfn "Test 3: %d" (intAdd "5,12,18,20,25,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250,260,270,280,290,300")
+printfn "Test 4: %d" (intAdd "30,55")
+printfn "Test 5: %d" (intAdd "15")
 
 // Technical Justification of the Solution
 
-// I decided to emphasize simplicity because the function will only
-// receive 0, one, or two elements. It's not worth using functions or loops 
-// for just one sum.
+// In this second task, I kept the approach simple by using built-in language functions, maintained the split, 
+// which is always necessary, and performed the subsequent operations with map and sum. This resulted in short, 
+// simple, and easy-to-understand code.
